00cef93dd7f704790dbcf5eac22fe5f81120b31a
Drvyn <Drvyn.in@gmail.com>
2025-07-03 17:16:20 +0530

    update

--- app/main.py
+++ app/main.py
@@ -17,15 +17,15 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 
 # CORS Configuration
+# In main.py
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://www.drvyn.in",
-        "https://drvyn-frontend.vercel.app  ",
+        "https://drvyn-frontend.vercel.app",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:8000",
-        "https://drvyn-backend.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],

d6ec90589d9190dadb6e1689d07b09a019e650d2
Drvyn <Drvyn.in@gmail.com>
2025-07-03 13:48:01 +0530

    update

--- app/routes/car.py
+++ app/routes/car.py
@@ -184,8 +184,6 @@ async def submit_request(request: CarRequest, response: Response):
             "createdAt": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
 
         }
-        
-        # Manually set CORS headers
         response.headers["Access-Control-Allow-Origin"] = "*"
         response.headers["Access-Control-Allow-Methods"] = "POST"
         response.headers["Access-Control-Allow-Headers"] = "Content-Type"
